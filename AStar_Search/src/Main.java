3072de21359e497172f6cc19f7bfab38a5ccfd03
Norumai01 <johnnydinhnguyen2001@gmail.com>
2023-06-13 15:03:11 -0400

    Putting the game into a loop until exit.

--- AStar_Search/src/Main.java
+++ AStar_Search/src/Main.java
@@ -1,12 +1,23 @@
+import java.util.*;  
+
 public class Main {
         public static void main(String[] args) {
         AStar game = new AStar();
-        game.generateMap();
-        System.out.println("Here is the Current Map. 0 is traverseable and 1 is untraverseable. ");
-        game.printMap();
-        game.CoordinateInput();
-        System.out.println("Current Map");
-        game.printMap();
-        game.runAStar();
+        Scanner user = new Scanner(System.in);
+        String input = "Yes";
+        
+        // Take this out of a while loop to work.
+        while (input.equals("Yes") || input.equals("yes") || input.equals("y") || input.equals("Y")) {
+            game.generateMap();
+            System.out.println("Here is the Current Map. 0 is traverseable and 1 is untraverseable. ");
+            game.printMap();
+            game.CoordinateInput();
+            System.out.println("Current Map");
+            game.printMap();
+            game.runAStar();
+            System.out.println("Would you like to run the game again? Enter Yes or No.");
+            input = user.nextLine();
+        }
+        user.close();
     }
 }
