4b70179c26155a436ccd08b6e57cfa96120d89ac
Norumai01 <johnnydinhnguyen2001@gmail.com>
2023-06-13 21:11:18 -0400

    Updated user prompt to continue or not.

--- AStar_Search/src/Main.java
+++ AStar_Search/src/Main.java
@@ -7,16 +7,21 @@ public class Main {
         String userInput = "Yes";
 
         game.generateMap();
-        // Take this out of a while loop to work.
-        // Any other words other than the argument will close the program. 
-        while (userInput.equals("Yes") || userInput.equals("yes") || userInput.equals("y") || userInput.equals("Y")) {   
-            System.out.println("Here is the Current Map. 0 is traverseable and 1 is untraverseable. ");
-            game.printMap();
-            game.CoordinateInput();
-            System.out.println("Current Map");
-            game.printMap();
-            game.runAStar();
-            System.out.println("Would you like to run the game again? Enter Yes or No.");
+        while(true) {
+            if (userInput.equals("Yes") || userInput.equals("yes") || userInput.equals("y") || userInput.equals("Y")) {
+                System.out.println("Here is the Current Map. 0 is traverseable and 1 is untraverseable. ");
+                game.printMap();
+                game.CoordinateInput();
+                System.out.println("Current Map");
+                game.printMap();
+                game.runAStar();
+                System.out.println("Would you like to run the game again? Enter Yes or No.");
+            } else if (userInput.equals("No") || userInput.equals("no") || userInput.equals("n") || userInput.equals("N")) {
+                System.out.println("Exiting the program...");
+                break;
+            } else {
+                System.out.println("Wrong input. To try again, enter Yes. Otherwise, No.");
+            }
             userInput = user.nextLine();
         }
         user.close();
